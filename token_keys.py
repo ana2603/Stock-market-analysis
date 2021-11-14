30f5b070fe27c873cc4b64c22c170c13ae4cd40f
ana2603 <85668517+ana2603@users.noreply.github.com>
2021-11-14 12:55:15 +0530

    Delete token_keys.py

--- token_keys.py
+++
@@ -1,7 +0,0 @@
-
-access_token = '1454395339457269761-XNdgOXP5mQxSmWzeKgCvaCgMzNkYKQ'
-access_token_secret = 'FSQOdHOzsYJKgHRBTmMQK6ykxG72N2zkgdKfCoD07UWmc'
-consumer_key = 'kLYgSQpVC1XQgiJDajLKvBpgq'
-consumer_secret = 'wdkatbBcraGSkGPCA1NyE1wsu9aAFAHoC2ImrHGC8D7z1rg1rC'
-
-
